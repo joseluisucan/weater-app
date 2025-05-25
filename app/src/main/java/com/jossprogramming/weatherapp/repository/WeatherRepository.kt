5701cbcf81839eb928ac3804166ac4d15f01748e
Jose Luis Pino Ucan <jose.ucan212hotmail.com>
2025-05-24 21:34:05 -0600

    Fix - Validation Null Pointer in Repository

--- app/src/main/java/com/jossprogramming/weatherapp/repository/WeatherRepository.kt
+++ app/src/main/java/com/jossprogramming/weatherapp/repository/WeatherRepository.kt
@@ -12,26 +12,34 @@ class WeatherRepository @Inject constructor(
     suspend fun getWeatherByZip(zipCode: String): Result<WeatherModelResponse> {
         return try {
             val response = weatherService.getWeatherByZip(Constanst.API_KEY, zipCode)
-            if (response.isSuccessful && response.body() != null) {
-                Result.success(response.body()!!)
+            val body = response.body()
+            val temperature = body?.current?.temperature
+
+            if (response.isSuccessful && body != null && temperature != null && temperature != 0) {
+                Result.success(body)
             } else {
-                Result.failure(Exception("Error en respuesta: ${response.message()}"))
+                Result.failure(Exception("Temperature not Available"))
             }
         } catch (e: Exception) {
-            Result.failure(e)
+            e.printStackTrace()
+            Result.failure(Exception("An unexpected error occurred"))
         }
     }
 
     suspend fun getWeatherByCoordinates(latLon: String): Result<WeatherModelResponse> {
         return try {
             val response = weatherService.getWeatherByCoordinates(Constanst.API_KEY, latLon)
-            if (response.isSuccessful && response.body() != null) {
-                Result.success(response.body()!!)
+            val body = response.body()
+            val temperature = body?.current?.temperature
+
+            if (response.isSuccessful && body != null && temperature != null && temperature != 0) {
+                Result.success(body)
             } else {
-                Result.failure(Exception("Error en respuesta: ${response.message()}"))
+                Result.failure(Exception("Temperature not Available"))
             }
         } catch (e: Exception) {
-            Result.failure(e)
+            e.printStackTrace()
+            Result.failure(Exception("An unexpected error occurred"))
         }
     }
 }
